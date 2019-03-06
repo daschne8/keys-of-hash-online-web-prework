36a7872594fb0810cbd4507e3e116f09277298ad
Daniel Schneider <daschne8@gmail.com>
2019-03-06 15:25:26 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(arguments)
     animals = []
     arguments.each do |location|
-      Hash.each do |hash_animal,hash_location|
+      self.each do |hash_animal,hash_location|
         if hash_location == location
           animals << hash_animal
         end
